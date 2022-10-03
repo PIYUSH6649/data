4fb73f8367d917b4f6a1fb5aeb9097c5845f83f2
PIYUSH6649 <100077733+PIYUSH6649@users.noreply.github.com>
2022-10-03 22:18:04 +0530

    Add files via upload

---
+++ duplicate.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *arr, sz;
+    int i, j;
+    int c = 0;
+    scanf("%d", &sz);
+    arr = (int *)malloc(sz * sizeof(int));
+    for (i = 0; i < sz; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < sz; i++)
+    {
+        for (j = 0; j < sz; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                c = c + 1;
+            }
+        }
+        if (c == 2)
+        {
+            printf("%d", arr[i]);
+        }
+        }
+    return 0;
+}
